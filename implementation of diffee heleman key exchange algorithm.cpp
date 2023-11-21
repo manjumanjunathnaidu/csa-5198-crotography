f2d5160f5e295e38da7b67f07e316aa7aa373c44
manjumanjunathnaidu <124764219+manjumanjunathnaidu@users.noreply.github.com>
2023-11-21 09:44:56 +0530

    Add files via upload

---
+++ implementation of diffee heleman key exchange algorithm.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+long long int power (int a, int b, int mod)
+{
+	long int t;
+	if(b==1)
+	return a;
+	t=power(a,b/2,mod);
+	if(b%2==0)
+	return (t*t)%mod;
+	else
+	return (((t*t)%mod)*a)%mod;
+} 
+long int calculatekey (int a, int x, int n)
+{
+	return power(a,x,n);
+}
+int main()
+{
+	int n,g,x,a,y,b;
+	printf("enter the value of n and g :");
+	scanf("%d",&n,&g);
+	printf("enter the value of x for the first person : ");
+	scanf("%d",&x);
+	a=power(g,x,n);
+	printf("enter the value of y for the second person :");
+	scanf("%d",&y);
+	b=power(g,y,n);
+	printf("key for the first person is : %11d\n",power(b,x,n));
+	printf("key for the second person is : %11d\n",power(a,y,n));
+	getch();
+	
+}
